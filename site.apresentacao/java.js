c964dfca99c1d109e44f17c0484ecc8548f7ae20
hey-afro <heyafroo@gmail.com>
2025-03-18 22:41:04 -0300

    att6

--- site.apresentacao/java.js
+++ site.apresentacao/java.js
@@ -2,11 +2,11 @@ function seta(){
     var a = document.getElementById('bottom')
     var nav = document.getElementById('mobile-redesocial')
 
-    if(a.style.color === 'white'){
-        a.style.color='red'
+    if(a.style.color === 'rgb(137, 137, 137)'){
+        a.style.color='rgb(131, 0, 0)'
         nav.style.display='flex'
     } else{
-        a.style.color='white'
+        a.style.color='rgb(137, 137, 137)'
         nav.style.display='none'
     }
 }
